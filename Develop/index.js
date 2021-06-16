e463b5a96e76e17f8299b494f910546e7fbdd9d9
jerry <jerry4905@hotmail.com>
2021-06-15 20:26:34 -0700

    added fs, inquierer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "userName"
+    },
+    {
+        type: "input",
+        message: "What Github repo would you like this README to be for?",
+        name: "repoName"
+    },
+    {
+        type: "input",
+        message: "What is your README Project Title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please enter a description for your project.",
+        name: "description"
+    },
+    {
+        type: "confirm",
+        message: "Would you like to add installation steps?",
+        name: "installation"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
